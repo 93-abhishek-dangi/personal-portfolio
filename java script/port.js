85360dc4ed27ba69059e6ca2f86993ab6396adc3
Abhishek Dangi <123855669+93-abhishek-dangi@users.noreply.github.com>
2023-03-06 22:58:31 +0530

    code uploaded
    personal portfolio using HTML, CSS And Java Script

---
+++ java script/port.js
@@ -0,0 +1,51 @@
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for (let i = 0; i<totalNavList; i++) {
+
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function () {
+
+            for (let i = 0; i < totalSection; i++) {
+                     allSection[i].classList.remove("back-section");
+                 }
+
+            for (let j = 0; j<totalNavList; j++) {
+                if (navList[j].querySelector("a").classList.contains("active")) {
+                //   console.log("back-section" + navList[j].querySelector("a"));
+                  // addBackSection(j);
+                     allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+                this.classList.add("active")
+                showSection(this);
+            }
+        })
+        
+    }
+    function showSection(element) {
+
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+       
+       //  console.log(target);
+        document.querySelector("#" + target).classList.add("active")
+    
+    }
+
+    const navTogglerBtn = document.querySelector(".nav-toggler");
+const aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+})
+function asideSectionTogglerBtn() {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i = 0; i<totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
+}
